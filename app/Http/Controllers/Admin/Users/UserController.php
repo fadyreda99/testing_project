ade1d10372be06d9f2ff132a22281ee018749d8e
fady-ui <fady.r.andrawes@gmail.com>
2025-01-08 21:41:55 +0200

    test find or and find or fail

--- app/Http/Controllers/Admin/Users/UserController.php
+++ app/Http/Controllers/Admin/Users/UserController.php
@@ -97,14 +97,21 @@ class UserController extends Controller
 
         //    upsert
 
-        $users = [
-            ['name' => 'test1update', 'email' => 'test1@gmail.com', 'password' => '123456789000'],
-            ['name' => 'test4', 'email' => 'test4@gmail.com', 'password' => '123456789'],
-        ];
-        User::upsert($users, ['email'], ['name', 'password']);
+        // $users = [
+        //     ['name' => 'test1update', 'email' => 'test1@gmail.com', 'password' => '123456789000'],
+        //     ['name' => 'test4', 'email' => 'test4@gmail.com', 'password' => '123456789'],
+        // ];
+        // User::upsert($users, ['email'], ['name', 'password']);
         //attribute internal state
         // dump($user->wasChanged());
         // dump($user->isDirty());
         // dump($user->isClean());
+
+        // find or && find or fail
+        // $user =  User::findOr(100, function () {
+        //     return "user not found";
+        // });
+        $user =  User::findOrFail(100);
+        dump($user);
     }
 }
